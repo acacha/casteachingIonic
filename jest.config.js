d9ccc28cfe200de451b008843745535fffd84253
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-11-23 09:36:04 +0100

    Removed typescript 2

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  preset: '@vue/cli-plugin-unit-jest/presets/default',
   transform: {
     '^.+\\.vue$': 'vue-jest'
   },
