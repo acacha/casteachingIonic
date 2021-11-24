a8fdca9c6eef5d8cb1d18a32d57e6059ef7658dc
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-11-24 19:37:40 +0100

    Primera versió de Show Video

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -5,31 +5,55 @@
         <ion-buttons slot="start">
           <ion-menu-button color="primary"></ion-menu-button>
         </ion-buttons>
-        <ion-title>Video {{ $route.params.id }}</ion-title>
+        <ion-title>{{ video.title }}</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Video {{ $route.params.id }}</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
-        <strong class="capitalize"> Video {{ $route.params.id }}</strong>
 
-        <p> TODO VIDEO</p>
+      <div id="container">
+        <iframe id="video" :src="video.url" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <ion-card>
+          <ion-card-header>
+            <ion-card-title>{{ video.title }}</ion-card-title>
+            <ion-card-subtitle>{{ video.published_at }}</ion-card-subtitle>
+          </ion-card-header>
+
+          <ion-card-content>
+            {{ video.description }}
+          </ion-card-content>
+        </ion-card>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import casteaching from '@acacha/casteaching'
+
+import {
+  IonButtons,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/vue';
+
 
 export default {
-  name: 'Folder',
+  name: 'Video',
   components: {
     IonButtons,
     IonContent,
@@ -37,34 +61,37 @@ export default {
     IonMenuButton,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent
+  },
+  data() {
+    return {
+      video: {}
+    }
+  },
+  async created() {
+    this.video = await casteaching.video.show(this.$route.params.id)
+
+    // this.video = {
+    //   id: 1,
+    //   title: "Ubuntu 101",
+    //   description: "# Here description",
+    //   url: "https://www.youtube.com/embed/w8j07_DBl_I",
+    //   published_at: "2020-12-13T20:00:00.000000Z",
+    // }
   }
 }
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+
+#video {
+  width:100%;
+  height:70vh;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
