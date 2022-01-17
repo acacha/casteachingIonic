ccde431c94fea97181712f8cff4afb92c201ecbf
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2022-01-17 15:26:03 +0100

    Codi final vídeo 105. Millorada la experiència d'usuari amb loading states i skeleton

--- src/views/Videos.vue
+++ src/views/Videos.vue
@@ -1,24 +1,122 @@
 <template>
-  <ion-list>
-    <ion-list-header>
-      Videos
-    </ion-list-header>
+  <ion-content>
+    <ion-refresher slot="fixed" @ion-refresh="refresh" id="refresher">
+      <ion-refresher-content></ion-refresher-content>
+    </ion-refresher>
+    <ion-list id="skeleton" v-if="loading">
+      <ion-list-header>
+        <ion-skeleton-text animated style="width: 80px"></ion-skeleton-text>
+      </ion-list-header>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-avatar slot="start">
+          <ion-skeleton-text></ion-skeleton-text>
+        </ion-avatar>
+        <ion-label>
+          <h3>
+            <ion-skeleton-text animated style="width: 80%"></ion-skeleton-text>
+          </h3>
+          <p>
+            <ion-skeleton-text animated style="width: 60%"></ion-skeleton-text>
+          </p>
+        </ion-label>
+      </ion-item>
+    </ion-list>
+    <ion-list v-else>
+      <ion-list-header>
+        Videos
+      </ion-list-header>
 
-    <ion-item v-for="video in videos" :key="video.id">
-      <ion-avatar slot="start">
-        <img src="https://via.placeholder.com/80">
-      </ion-avatar>
-      <ion-label>
-        <h2>{{ video.title }}</h2>
-        <p>{{ video.description }}</p>
-      </ion-label>
-    </ion-item>
+      <ion-item v-for="video in videos" :key="video.id">
+        <ion-avatar slot="start">
+          <img src="https://via.placeholder.com/80">
+        </ion-avatar>
+        <ion-label>
+          <h2>{{ video.title }}</h2>
+          <p>{{ video.description }}</p>
+        </ion-label>
+      </ion-item>
 
-  </ion-list>
+    </ion-list>
+  </ion-content>
 </template>
 
 <script>
-import {IonAvatar, IonItem, IonLabel, IonList, IonListHeader} from "@ionic/vue";
+import {
+  IonAvatar,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonRefresher,
+  IonRefresherContent,
+  IonContent,
+  IonSkeletonText
+} from "@ionic/vue";
 import casteaching from '@acacha/casteaching'
 
 export default {
@@ -28,34 +126,33 @@ export default {
     IonItem,
     IonListHeader,
     IonAvatar,
-    IonLabel
+    IonLabel,
+    IonRefresher,
+    IonRefresherContent,
+    IonContent,
+    IonSkeletonText,
   },
   data() {
     return {
-      videos: [
-        // {
-        //   id: 1,
-        //   title: "Ubuntu 101",
-        //   description: "# Here description",
-        //   url: "https://www.youtube.com/embed/w8j07_DBl_I",
-        // },
-        // {
-        //   id: 2,
-        //   title: "Ubuntu 102",
-        //   description: "# Here description",
-        //   url: "https://www.youtube.com/embed/w8j07_DBl_I",
-        // },
-        // {
-        //   id: 3,
-        //   title: "Ubuntu 103",
-        //   description: "# Here description",
-        //   url: "https://www.youtube.com/embed/w8j07_DBl_I",
-        // },
-      ]
+      videos: [],
+      loading: true
     }
   },
   async created() {
-    this.videos = await casteaching.videos()
+    await this.fetchVideos()
+    this.loading = false
+  },
+  mounted() {
+    this.refresher = document.getElementById('refresher');
+  },
+  methods: {
+    async refresh() {
+      await this.fetchVideos()
+      this.refresher.complete();
+    },
+    async fetchVideos() {
+      this.videos = await casteaching.videos()
+    }
   }
 }
 </script>
